c190822b85cfba97281231985d4f67aea8fb511d
stvnmc <stvnmc123@gmail.com>
2024-05-23 15:12:55 -0600

    creando formulario

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
 
-export function GET(){
-    return NextResponse.json("creando tareas")
+export function GET() {
+  return NextResponse.json("creando tareas");
+}
+
+export async function POST(req) {
+  const { title, description } = await req.json(); // Asegúrate de parsear el body del request
+  // Aquí puedes agregar lógica para manejar la creación de una nueva tarea
+  return NextResponse.json({ message: "Tarea creada", title, description });
 }
--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const New = () => {
-  return (
-    <div>New</div>
-  )
-}
+  const router = useRouter();
 
-export default New
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">Titulo de la tarea</label>
+        <input type="text" id="title" placeholder="Titulo" />
+
+        <label htmlFor="description">Descripcion de la tarea</label>
+        <textarea
+          rows="3"
+          id="description"
+          placeholder="Describe tu tarea"
+        ></textarea>
+
+        <button>Crear</button>
+      </form>
+    </div>
+  );
+};
+
+export default New;
